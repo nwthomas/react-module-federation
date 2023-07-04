402d6e626ecfb3bb892a9f3b1f517406d1691b01
Nathan Thomas <nwthomas@me.com>
2023-07-04 00:20:39 -0700

    Add Webpack configs to Container app

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  node: {
+    __dirname: true,
+    __filename: true,
+    global: true,
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"), // Specify the output directory path
+    filename: "main.js", // Specify the output bundle filename
+  },
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+const mergedConfig = merge(commonConfig, devConfig);
+
+module.exports = mergedConfig;
