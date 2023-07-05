9d416238700bb1ffe45bf60f99a2d0066ba684f4
Nathan Thomas <nwthomas@me.com>
2023-07-04 23:25:01 -0700

    Make various updates for auth package

---
+++ packages/auth/src/App.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { Route, Router, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
+function App({ history }) {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch></Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
