1fa5825dbad694fffe7df886ee72e86e8c549f5e
Nathan Thomas <nwthomas@me.com>
2023-07-05 11:22:12 -0700

    Remove s from import module federation line

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -20,7 +20,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        auth: `auth@${domain}/auth/latests/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
